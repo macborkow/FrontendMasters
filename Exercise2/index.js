f9654f66de08bb5ac5e2125a5c1d54e84b6bdb32
macborkow <macborkow@gmail.com>
2021-04-19 11:57:05 +0200

    Add minus value operation functionality

--- Exercise2/index.js
+++ Exercise2/index.js
@@ -1,4 +1,7 @@
 let result = "0";
+let operator = "";
+let makeMinus = false;
+let resultShown = false;
 
 window.onload = () => {
   let screen = document.querySelector(".screen");
@@ -10,25 +13,91 @@ window.onload = () => {
       switch (item.innerHTML) {
         case "←":
           item.addEventListener("click", () => {
-            if (screen.innerHTML.length > 1) {
-              screen.innerHTML = screen.innerHTML.replace(/.$/, "");
-            } else {
-              screen.innerHTML = "0";
+            if (!resultShown) {
+              if (screen.innerHTML.length > 1) {
+                screen.innerHTML = screen.innerHTML.replace(/.$/, "");
+              } else {
+                screen.innerHTML = "0";
+              }
             }
           });
           break;
         case "C":
           item.addEventListener("click", () => {
             screen.innerHTML = "0";
+            result = "0";
+            resultShown = false;
           });
           break;
+        case "=":
+          item.addEventListener("click", () => {
+            if (!resultShown) {
+              switch (operator) {
+                case "+":
+                  result = (
+                    parseInt(result) + parseInt(screen.innerHTML)
+                  ).toString();
+                  screen.innerHTML = result;
+                  resultShown = true;
+                  operator = "";
+                  break;
+                case "-":
+                  result = (
+                    parseInt(result) - parseInt(screen.innerHTML)
+                  ).toString();
+                  screen.innerHTML = result;
+                  resultShown = true;
+                  operator = "";
+                  break;
+                case "÷":
+                  result = Math.floor(
+                    parseInt(result) / parseInt(screen.innerHTML)
+                  ).toString();
+                  screen.innerHTML = result;
+                  resultShown = true;
+                  operator = "";
+                  break;
+                case "×":
+                  result = (
+                    parseInt(result) * parseInt(screen.innerHTML)
+                  ).toString();
+                  screen.innerHTML = result;
+                  resultShown = true;
+                  operator = "";
+                  break;
+              }
+            }
+          });
+          break;
+        default:
+          item.addEventListener("click", () => {
+            if (operator !== "-" && screen.innerHTML === "0") {
+              makeMinus = true;
+            } else operator = item.innerHTML;
+
+            if (result === "0") {
+              result = screen.innerHTML;
+              resultShown = true;
+            }
+
+            if (resultShown) {
+              screen.innerHTML = "0";
+              resultShown = false;
+            }
+          });
       }
     } else {
       item.addEventListener("click", () => {
-        if (screen.innerHTML === "0") {
-          screen.innerHTML = item.innerHTML;
-        } else {
-          screen.innerHTML += item.innerHTML;
+        if (!resultShown) {
+          if (screen.innerHTML === "0") {
+            if (makeMinus) {
+                screen.innerHTML = -item.innerHTML;
+                makeMinus = false;
+            }
+            else screen.innerHTML = item.innerHTML;
+          } else {
+            screen.innerHTML += item.innerHTML;
+          }
         }
       });
     }
