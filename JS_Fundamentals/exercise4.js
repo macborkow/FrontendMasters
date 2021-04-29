5b7eb1dfd5dc0068a6d60d494035ca7351bbd4ff
macborkow <macborkow@gmail.com>
2021-04-29 09:24:46 +0200

    Add forEach implementation exercise

---
+++ JS_Fundamentals/exercise4.js
@@ -0,0 +1,23 @@
+let suspects = [ "Mr. White", "Ms. Scarlet", "Col. Mustard" ];
+
+let createSuspect = (suspect) => ({
+	name: suspect,
+	color: suspect.split(" ")[1],
+	say() 
+	{
+		console.log(`hi i am ${suspect}`);
+	}
+});
+
+let _ = {
+	each(list, callback) {
+		if(Array.isArray(list))
+			for(let item of list) {
+				callback(item);
+			}
+		else
+			for(let item in list) {
+				callback(item);
+			};
+	}
+};
