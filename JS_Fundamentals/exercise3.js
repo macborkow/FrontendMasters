1784b3b33585d2914a806d330c5be329414944d4
macborkow <macborkow@gmail.com>
2021-04-29 08:20:49 +0200

    Add third JS fundamentals exercise

---
+++ JS_Fundamentals/exercise3.js
@@ -0,0 +1,14 @@
+let suspects = [
+	{
+		name: "Rusty",
+		color: "Orange"
+	},
+	{
+		name: "Ms. Scarlet",
+		color: "Red"
+	}
+];
+
+let [{color: firstColor}, {color: secondColor}] = suspects;
+
+console.log(firstColor, secondColor);
